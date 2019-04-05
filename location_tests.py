f826eae522f2a6705a53cffcece6f87a646a5a85
jjszeto <jjszeto@csc303x04.ad.calpoly.edu>
2019-04-05 12:38:08 -0700

    added eq tests

--- location_tests.py
+++ location_tests.py
@@ -8,6 +8,20 @@ class TestLab1(unittest.TestCase):
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
     
     # Add more tests!
+    def test_eq_1(self):
+        loc = Location("SLO", 35.3, -120.7)
+        loc1 = Location("SLO", 35.3, -120.7)
+        self.assertTrue(loc == loc1)
+
+    def test_eq_2(self):
+        loc = Location("SLO", 35.3, -120.7)
+        loc1 = Location("Paris", 48.9, 2.4)
+        self.assertFalse(loc == loc1)
+
+    def test_eq_3(self):
+        loc = Location("SLO", 35.3, -120.7)
+        loc1 = ("Paris", 48.9, 2.4)
+        self.assertFalse(loc == loc1)
 
 if __name__ == "__main__":
         unittest.main()
